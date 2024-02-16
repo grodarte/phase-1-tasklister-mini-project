aaeaaa0375bb2c1affb86ab4a82a35656ea2baab
Gabrielle <ms.grodarte@gmail.com>
2024-02-15 16:14:04 -0800

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    const form = document.getElementById(`create-task-form`)
+    
+    form.addEventListener(`submit`, e => {
+      e.preventDefault()
+      buildTodo(e.target[`new-task-description`].value)
+      form.reset()
+    })
 });
+
+function buildTodo(todo){
+  let li = document.createElement(`li`)
+  let btn = document.createElement(`button`)
+  btn.addEventListener(`click`, handleDelete)
+  li.textContent = todo
+  btn.textContent = `x`
+
+  li.appendChild(btn)
+  
+  document.getElementById(`tasks`).appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
